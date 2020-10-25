dabc74aa7ad9ed53cb01daf97b643dfcc818c0c0
Eduardo Spagna <eduardo@gempe.dev>
2020-10-25 20:26:14 -0300

    Adiciona validação para autenticar um usuário

--- app/Validators/UserValidator.js
+++ app/Validators/UserValidator.js
@@ -12,6 +12,13 @@ class UserValidator {
     };
   }
 
+  static get rulesAuthentication() {
+    return {
+      user_email: 'required|string|email',
+      user_password: 'required|string|min:8|max:30',
+    };
+  }
+
   static get messagesCreate() {
     return {
       'user_name.required': UserMessage.getMessage('user/required-name'),
@@ -32,8 +39,24 @@ class UserValidator {
     };
   }
 
+  static get messagesAuthentication() {
+    return {
+      'user_email.required': UserMessage.getMessage('user/required-email'),
+      'user_email.string': UserMessage.getMessage('user/data-type-string-email'),
+      'user_email.email': UserMessage.getMessage('user/invalid-email'),
+      'user_password.required': UserMessage.getMessage('user/required-password'),
+      'user_password.string': UserMessage.getMessage('user/data-type-string-password'),
+      'user_password.min': UserMessage.getMessage('user/min-limit-password'),
+      'user_password.max': UserMessage.getMessage('user/max-limit-password'),
+    };
+  }
+
   static async validationCreate(data) {
     return validate(data, this.rulesCreate, this.messagesCreate);
+  }
+
+  static async validationAuthentication(data) {
+    return validate(data, this.rulesAuthentication, this.messagesAuthentication);
   }
 }
 
